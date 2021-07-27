b31a5124b2a86887f5a63b070c56481022c5727d
MIRJAXON-CODER <52111895+MIRJAXON-CODER@users.noreply.github.com>
2021-07-27 20:51:33 +0500

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -3,13 +3,15 @@
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
+if (!file_exists('users.db')) {
+    file_put_contents('users.db','{"userID":[]}');
+}
 include 'madeline.php';
 
 $MadelineProto = new \danog\MadelineProto\API('JD.madeline');
 $MadelineProto->async(true);
-$MadelineProto->loop(function () use ($MadelineProto) {
-     $MadelineProto->start();
-    $offset=0;
+
+while (true) {
     $updates = $MadelineProto->get_updates(['offset' => $offset, 'limit' => 50, 'timeout' => 0]);
     foreach ($updates as $update) {
         $offset = $update['update_id'] + 1;
@@ -21,7 +23,7 @@ $chatID = $chatID['bot_api_id'];
 $userID = $update['update']['message']['from_id'];
 $msg = $update['update']['message']['message'];
 $msg_id = $update['update']['message']['id'];
-$date=file_get_contents("send.txt");
+$date=file_get_contents("send$userID.txt");
 $list=json_decode(file_get_contents("users.db"));
 $users=$list->userID;
 if($msg){
@@ -35,32 +37,42 @@ if($msg){
 }
 
 if($msg == "/send"){
- 
-file_put_contents("send.txt","sett");
+ $Conf = json_decode(file_get_contents('Config.json'));
+$Conf->Enemy = 1;
+file_put_contents('Config.json', json_encode($Conf));
+file_put_contents("send$userID.txt","sett");
 $ed = $MadelineProto->messages->editMessage(['peer' => $chatID, 'id' => $msg_id, 'message' =>'Yubormoqchi bulgan xabaringizni yozing!  Bekor qilish uchun /cancelni yuboring!', 'parse_mode' => 'MarkDown' ]);
 }
 
 if($msg == "/cancel"&& $date=="sett"){
-file_put_contents("send.txt","");
+file_put_contents("send$userID.txt","");
+$Conf = json_decode(file_get_contents('Config.json'));
+$Conf->Enemy = 1;
 $ed = $MadelineProto->messages->editMessage(['peer' => $chatID, 'id' => $msg_id, 'message' =>'Yuborish Bekor qilindi! ', 'parse_mode' => 'MarkDown' ]);
 }
 elseif($msg && $date=="sett"){
    foreach ($users as $key => $value) {
-        $date=file_get_contents("send.txt");
+        $date=file_get_contents("send$userID.txt");
+        $Conf = json_decode(file_get_contents('Config.json'));
+        $Conf->Enemy = 1;
         $ed = $MadelineProto->messages->sendMessage(['peer' => $value, 'message' =>$msg, 'parse_mode' => 'MarkDown' ]);
         if($date=="sett")
-        $date=file_put_contents("send.txt","");
+        $date=file_put_contents("send$userID.txt","");
    }
 }
 
 
 if($msg == "/memb"){
+ $Conf = json_decode(file_get_contents('Config.json'));
+$Conf->Enemy = 1;
 $txs=count($users);
 $ed = $MadelineProto->messages->editMessage(['peer' => $chatID, 'id' => $msg_id, 'message' =>$txs, 'parse_mode' => 'MarkDown' ]);
 }
 
 
-        try {
+     
+                try {
+
 }catch(Exception $e){
     }
     catch(\danog\MadelineProto\RPCErrorException $e){
@@ -70,5 +82,7 @@ $ed = $MadelineProto->messages->editMessage(['peer' => $chatID, 'id' => $msg_id,
     }
     catch(\danog\MadelineProto\TL\Conversion\Exception $e){
     }
-});
+}
+}
+}
 ?>
