a18751666daeb2fa91bac33de53a5c7e5cfd110e
MIRJAXON-CODER <52111895+MIRJAXON-CODER@users.noreply.github.com>
2021-07-27 20:55:02 +0500

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -1,15 +1,64 @@
 <?php
-
+error_reporting(0);
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-if (!file_exists('users.db')) {
+if (!file_exists('Config.json')) {
+  file_put_contents('Config.json', '{"Monshi":0,"Markread":0,"Typing":0,"Poker":0,"Enemy":0}');
+}
+if (!file_exists('users.db')){
     file_put_contents('users.db','{"userID":[]}');
 }
-include 'madeline.php';
+define('MADELINE_BRANCH', 'deprecated');
+include "madeline.php";
+    function closeConnection($message = 'MCoder.Uz')
+{
+    if (php_sapi_name() === 'cli' || isset($GLOBALS['exited'])) {
+        return;
+    }
+    @ob_end_clean();
+    header('Connection: close');
+    ignore_user_abort(true);
+    ob_start();
+    echo "$message";
+    $size = ob_get_length();
+    header("Content-Length: $size");
+    header('Content-Type: text/html');
+    ob_end_flush();
+    flush();
+    $GLOBALS['exited'] = true;
+}
+function shutdown_function($lock)
+{
+    $a = fsockopen((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'tls' : 'tcp').'://'.$_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT']);
+    fwrite($a, $_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'].' '.$_SERVER['SERVER_PROTOCOL']."\r\n".'Host: '.$_SERVER['SERVER_NAME']."\r\n\r\n");
+    flock($lock, LOCK_UN);
+    fclose($lock);
+}
+if (!file_exists('bot.lock')) {
+    touch('bot.lock');
+}
+$lock = fopen('bot.lock', 'r+');
 
+$try = 1;
+$locked = false;
+while (!$locked) {
+    $locked = flock($lock, LOCK_EX | LOCK_NB);
+    if (!$locked) {
+        closeConnection();
+
+        if ($try++ >= 30) {
+            exit;
+        }
+        sleep(1);
+    }
+}
 $MadelineProto = new \danog\MadelineProto\API('JD.madeline');
-$MadelineProto->async(true);
+$MadelineProto->start();
+$offset = 0;
+
+register_shutdown_function('shutdown_function', $lock);
+closeConnection();
 
 while (true) {
     $updates = $MadelineProto->get_updates(['offset' => $offset, 'limit' => 50, 'timeout' => 0]);
@@ -73,6 +122,7 @@ $ed = $MadelineProto->messages->editMessage(['peer' => $chatID, 'id' => $msg_id,
      
                 try {
 
+    include 'function.php';
 }catch(Exception $e){
     }
     catch(\danog\MadelineProto\RPCErrorException $e){
